d4efd23eb73ccf45bf07d7cded67547ab80828e2
alisa.savuliak <alisa.savuliak@lobyco.com>
2021-08-24 14:12:43 +0300

    add appsettings

---
+++ CoreAutomationFramework/CoreAutomationFramework/Config/AppSettings.cs
@@ -0,0 +1,7 @@
+namespace CoreAutomationFramework.Config
+{
+    public class AppSettings
+    {
+        public string pathToConsoleApp { get; set; }
+    }
+}
--- CoreAutomationFramework/CoreAutomationFramework/Tests/BaseTest.cs
+++ CoreAutomationFramework/CoreAutomationFramework/Tests/BaseTest.cs
@@ -1,13 +1,25 @@
 using System;
-using System.Reflection;
+using System.IO;
+using CoreAutomationFramework.Config;
 using CoreAutomationFramework.Methods;
 using NUnit.Framework;
+using System.Text.Json;
 
 namespace CoreAutomationFramework.Tests
 {
     public class BaseTest
     {
-        protected string pathToDll => @"/Users/asa/RiderProjects/NmeaSentenceParser/NmeaSentenceParser/bin/Debug/net5.0/";
+        private AppSettings appSettings = new AppSettings();
+        protected string path => Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+
+        public AppSettings EnvironmentConfig()
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            var jsonFile = File.ReadAllText(path);
+            return  JsonSerializer.Deserialize<AppSettings>(jsonFile);
+        }
+
+        protected string pathToDll => EnvironmentConfig().pathToConsoleApp;
         MainMethods mainMethods => new MainMethods();
 
         [TearDown]
